0f8cb04c70499a3ae92ba787f90500c019b5295c
Kerolosnady1 <kerolosnady444@gmail.com>
2024-07-01 17:55:05 +0000

    0x03ES6_data_manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Kerolos Nady <https://github.com/Kerolosnady1>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
